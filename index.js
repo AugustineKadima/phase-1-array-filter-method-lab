78dc62d9dc7043b37b5ba42eb20da1c6745192f9
AugustineKadima <kadimasam@gmail.com>
2022-02-27 16:51:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(arr, nameToSearch){
+    let matchingArray = []
+    for(let name of arr){
+        if (name.toLowerCase() === nameToSearch.toLowerCase()) matchingArray.push(name)
+    }
+    return matchingArray
+}
+
+function fuzzyMatch(arr, item){
+    let matchingArray = []
+    for(let name of arr){
+        if (name.slice(0,2) === item) matchingArray.push(name)
+    }
+    return matchingArray
+}
+
+function matchName(arr, name){
+    let matchingArray = []
+    for(let item of arr){
+        if(item["name"] === name)
+        matchingArray.push(item)
+    }
+    return matchingArray
+}
+
+
